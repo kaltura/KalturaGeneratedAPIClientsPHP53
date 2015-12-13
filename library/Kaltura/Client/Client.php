674cd482a7b2872c6a1a9ac6f21706bc70edd1da
Kaltura CSI <community@kaltura.com>
2015-12-13 00:51:59 -0500

    New clientlib ver

--- library/Kaltura/Client/Client.php
+++ library/Kaltura/Client/Client.php
@@ -317,7 +317,7 @@ class Client extends Base
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:15-12-12');
+		$this->setClientTag('php5:15-12-13');
 		$this->setApiVersion('3.3.0');
 	}
 	
