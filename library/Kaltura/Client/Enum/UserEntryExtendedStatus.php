fad19cf567f1109647a0c24f6234a33d0745b0e2
Kaltura CSI <community@kaltura.com>
2022-10-24 08:29:24 -0400

    New clientlib ver

--- library/Kaltura/Client/Enum/UserEntryExtendedStatus.php
+++ library/Kaltura/Client/Enum/UserEntryExtendedStatus.php
@@ -38,6 +38,8 @@ namespace Kaltura\Client\Enum;
  */
 class UserEntryExtendedStatus extends \Kaltura\Client\EnumBase
 {
+	const SYNC_STATUS_ERROR = "quiz.SYNC_STATUS_ERROR";
+	const SYNC_STATUS_SUCCESS = "quiz.SYNC_STATUS_SUCCESS";
 	const PLAYBACK_COMPLETE = "viewHistory.PLAYBACK_COMPLETE";
 	const PLAYBACK_STARTED = "viewHistory.PLAYBACK_STARTED";
 	const VIEWED = "viewHistory.VIEWED";
